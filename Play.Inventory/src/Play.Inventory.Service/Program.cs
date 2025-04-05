07f270ee6f7ce1c0e132e5fe2b6a6def64cfbb8f
Ifegwu <iagbanyim@aservo.com>
2025-04-05 12:37:55 +0200

    Add a method to handle transient HTTP errors, that can handle multiple microservices calls.

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -29,6 +29,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMongo()
                 .AddMongoRepository<InventoryItem>("inventoryitems");
 
+Random jitterer = new Random();
+
 builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     client.BaseAddress = new Uri("https://localhost:5001");
@@ -44,6 +46,17 @@ builder.Services.AddHttpClient<CatalogClient>(client =>
     };
     return handler;
 })
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    5,
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                    + TimeSpan.FromMilliseconds(jitterer.Next(0, 10000))
+    // onRetry: (outcome, timespan, retryAttempt) =>
+    // {
+    //     var serviceProvider = builder.Services.BuildServiceProvider();
+    //     serviceProvider.GetService<ILogger<CatalogClient>>()?
+    //         .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+    // }
+))
 .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 // builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("RabbitMQSettings"));
