1695aa683df9b8daa5e52d110f0a7e6f163f93d7
Ifegwu <iagbanyim@aservo.com>
2025-04-08 16:16:40 +0200

    Integrate REST API for the Identity microservice that uses MongoDB as a backend, which will also be used by other microservices.

--- Play.Identity/src/Play.Identity.Service/Program.cs
+++ Play.Identity/src/Play.Identity.Service/Program.cs
@@ -52,8 +52,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
     app.MapOpenApi();
 }
+
 
 app.UseHttpsRedirection();
 
